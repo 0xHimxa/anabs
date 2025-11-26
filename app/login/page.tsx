1c100d95e2eb40229a97d4d7b16d7a0c78021791
himxa <himxa0x@gmail.com>
2025-11-26 21:50:00 +0100

    make the login page interactive

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 
 import "../general.css";
+import "./login.css"
 import "../f-home.css";
 import { FcGoogle } from "react-icons/fc";
 
@@ -15,21 +16,21 @@ export default function Login_Page(){
 
 
 <div className="bg-[#FFFFFF] h-[80%] w-[70%] pr-20 pl-10 pt-12 flex flex-col gap-15
- ">
+ login-box ">
 
     <h1 className=" text-2xl text-center">Welcome Back</h1>
 
 
 <form action="" className=" flex flex-col gap-4 h-[45%]">
 
-<div className=" mb-4 pt-3 pb-2  border-1 border-gray-400"><input type="text" placeholder="Enter Email" className="w-[100%] h-7 border-none rounded pr-2 pl-2 text-lg input-f"/></div>
-<div className=" pt-3 pb-2  border-1 border-gray-400"><input type="text" placeholder="Password" className="w-[100%] h-7 border-none rounded pr-2 pl-2 text-lg input-f"/></div>
+<div className=" mb-4 pt-3 pb-2  border-1 border-gray-400 input-box"><input type="text" placeholder="Enter Email" className="w-[100%] h-7 border-none rounded pr-2 pl-2 text-lg input-f"/></div>
+<div className=" pt-3 pb-2  border-1 border-gray-400 input-box"><input type="text" placeholder="Password" className="w-[100%] h-7 border-none rounded pr-2 pl-2 text-lg input-f"/></div>
 <div className=" flex justify-between">
-<div className="">
+<div className="after-form-input">
     <input type="checkbox" name="remberme" id="" />
     <span  className="text-base pl-1">Remember me</span>
 </div>
-<div>
+<div className="after-form-input">
     <a href="">Forget password</a>
 </div>
 
@@ -44,13 +45,13 @@ export default function Login_Page(){
 </form>
 
 <div className="flex justify-center w-[70%] ml-20 pb-3
-pt-3 rounded border-1 border-solid">
+pt-3 rounded border-1 border-solid google-box">
     <FcGoogle  className="self-center text-2xl"/>
 <span className="pl-2">Contiue with Google</span>
 
 </div>
 
-<div className="flex  justify-center relative bottom-10 ">
+<div className="flex  justify-center relative bottom-10 create-acc">
     <span>Don't have account <a href="">SignUp now?</a></span>
 </div>
 
